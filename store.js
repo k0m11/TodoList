e6d24b7e3b48a1fdc19b7420835536419d055cf1
rvchk <115169534+k0m11@users.noreply.github.com>
2024-08-30 21:41:14 +0300

    Update store.js

--- store.js
+++ store.js
@@ -1,110 +1,42 @@
-// 7:17:49
+// =============== ToDo Список Задач ===============
 
+todoList = document.querySelector(".list")
+todoForm = document.querySelector(".container-input")
+todoInput = document.querySelector("#input")
+AddButton = document.querySelector("#submit-btn")
 
-// ************ SELECT ITEMS ************
-let alert = document.querySelector(".alert")
-let grocery = document.querySelector(".list")
-let input = document.querySelector("#input")
-let submitBtn = document.querySelector("#submit-btn")
-let clearBtn = document.querySelector("#clear-btn")
+todoForm.addEventListener("submit", formHandler)
 
-// edit option
-let editElement;
-let editFlag = false;
-let editID = "";
-// ************ EVENT LISTENERS ************
-submitBtn.addEventListener("click", addItem)
-clearBtn.addEventListener("click", clearAll)
+function formHandler(event) {
+    // Чтобы Страница не перезагружалась при нажатии на кнопку
+    event.preventDefault()
 
-function addItem(e) {
-    e.preventDefault();
-    let value = input.value
-    value = valueDecor(value)
-    const id = new Date().getTime().toString() // some kind of random numbers
+    // Получаем название задачи из инпута
+    const taskText = todoInput.value
 
-    if(value !== "" && editFlag == false) {
-        const element = document.createElement("article")
-        element.classList.add("list-item")
-        const attribute = document.createAttribute("data-id")
-        attribute.value = id
-        element.setAttributeNode(attribute)
-        element.innerHTML = `
-                <p class="title">${value}</p>
-                <div class="btn-container">
-                    <button type="button" class="edit-btn">
-                    </button>
-                    <button type="button" class="delete-btn">
-                    </button>
-                </div>
-        `
-        // append child
-        grocery.appendChild(element)
-        // item buttons
-        const deleteBtn = document.querySelector(".delete-btn")
-        const editBtn = document.querySelector(".edit-btn")
+    // Создание тега <li> с помощью CreateElement
+    const newTask = document.createElement("li")
+    newTask.className = "list-item"
+    newTask.innerHTML = taskText
 
-        deleteBtn.addEventListener("click", deleteItem)
-        editBtn.addEventListener("click", editItem)
+    // Создание кнопки Удалить с помощью CreateElement
+    const deleteBtn = document.createElement("button")
+    deleteBtn.className = "delete-btn"
+    deleteBtn.setAttribute("role", "button")
+    newTask.append(deleteBtn)
 
-        displayAlert("Item Added to the list", "correct")
-        setBackToDefault()
-        clearBtn.style.display = "inline"
-        grocery.style.display = "block"
-        
-    }
-    else if(value !== "" && editFlag) {
-        
-    }
-    else {
-        displayAlert("Please Enter Value", "wrong")
-    }
-}
+    // Добавляем событие по клику
+    
+    deleteBtn.addEventListener("click", function () {
+        this.closest("li").remove()
+    })
 
-function addToLocalStorage(value, id) {
-    // will add soon
-}
+    // Добавляем Элемент на страницу
+    todoList.append(newTask)
 
-function displayAlert(text, action) {
-    alert.innerHTML = text
-    alert.classList.add(`alert-${action}`)
+    // Очищаем поле ввода
+    todoInput.value = ""
 
-    setTimeout(function() {
-        alert.innerHTML = ""
-        alert.classList.remove(`alert-${action}`)
-    },1000)
-}
-
-function setBackToDefault() {
-    input.value = ""
-    editFlag = false
-    editID = ""
-    submitBtn.innerHTML = "submit"
-}
-
-function deleteItem(e) {
-    const element = e.currentTarget.parentElement.parentElement
-    grocery.removeChild(element)
-    if(grocery.children.length === 0) {
-        clearBtn.style.display = "none"
-    }
-    displayAlert("Item Removed", "wrong")
-    setBackToDefault()
-}
-
-function editItem() {
-    // will add soon
-}
-
-function clearAll() {
-    if(confirm("Clear All Items???")) {
-        grocery.innerHTML = ""
-        clearBtn.style.display = "none"
-        displayAlert("Empty List", "wrong")
-        setBackToDefault()
-        // localStorage.removeItem("list")
-    }
-}
-
-function valueDecor(x) {
-    return x.split(" ").map((y)=> y[0].toUpperCase() + y.slice(1)).join(" ")
+    // Фокус на поле ввода, если нажали на кнопку
+    todoInput.focus()
 }
