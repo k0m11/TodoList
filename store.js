74fa271d991e8bc02e114d0fe09e8863dbd6b4d8
rvchk <115169534+k0m11@users.noreply.github.com>
2024-08-31 11:13:40 +0300

    Update store.js

--- store.js
+++ store.js
@@ -4,39 +4,50 @@ todoList = document.querySelector(".list")
 todoForm = document.querySelector(".container-input")
 todoInput = document.querySelector("#input")
 AddButton = document.querySelector("#submit-btn")
+todoAlert = document.querySelector(".alert")
 
 todoForm.addEventListener("submit", formHandler)
 
 function formHandler(event) {
+    if (todoInput.value !== "") {
+
     // Чтобы Страница не перезагружалась при нажатии на кнопку
     event.preventDefault()
 
     // Получаем название задачи из инпута
     const taskText = todoInput.value
 
-    // Создание тега <li> с помощью CreateElement
-    const newTask = document.createElement("li")
-    newTask.className = "list-item"
-    newTask.innerHTML = taskText
-
-    // Создание кнопки Удалить с помощью CreateElement
-    const deleteBtn = document.createElement("button")
-    deleteBtn.className = "delete-btn"
-    deleteBtn.setAttribute("role", "button")
-    newTask.append(deleteBtn)
-
-    // Добавляем событие по клику
-    
-    deleteBtn.addEventListener("click", function () {
-        this.closest("li").remove()
-    })
+    // Создание тега <li>
+    const newTask = `
+        <li class="list-item"> ${taskText}
+            <div class="buttons">
+                <button class="delete-btn"></button>
+                <button class="edit-btn"></button>
+            </div>
+        </li>`;
 
     // Добавляем Элемент на страницу
-    todoList.append(newTask)
+    todoList.innerHTML += newTask
+
+    editBtn = document.querySelectorAll(".edit-btn")
+    editBtn.forEach(element => element.addEventListener("click", editItem))
+
+    deleteBtn = document.querySelectorAll(".delete-btn")
+    deleteBtn.forEach(element => element.addEventListener("click", deleteItem))
 
     // Очищаем поле ввода
     todoInput.value = ""
 
     // Фокус на поле ввода, если нажали на кнопку
     todoInput.focus()
+    }
+    else return;
+}
+
+function deleteItem() {
+    console.log(this.closest("li").remove())
+}
+
+function editItem() {
+
 }
